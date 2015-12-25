d8a31d1bbf0e373131fa7bb6344486cf1359edb1
franciscofranco <franciscofranco.1990@gmail.com>
2015-12-25 17:54:33 +0000

    cpufreq: cpu-boost: export input_boost_enable to userspace
    Users should have the choice to run touchboost-less (even though it ruins performance)
    
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    Signed-off-by: msfkonsole <msfkonsole@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -51,7 +51,8 @@ module_param(boost_ms, uint, 0644);
 static unsigned int sync_threshold;
 module_param(sync_threshold, uint, 0644);
 
-static bool input_boost_enabled;
+static unsigned int input_boost_enabled = 1;
+module_param(input_boost_enabled, uint, 0644);
 
 static unsigned int input_boost_ms = 40;
 module_param(input_boost_ms, uint, 0644);
@@ -65,7 +66,6 @@ static int set_input_boost_freq(const char *buf, const struct kernel_param *kp)
 	int i, ntokens = 0;
 	unsigned int val, cpu;
 	const char *cp = buf;
-	bool enabled = false;
 
 	while ((cp = strpbrk(cp + 1, " :")))
 		ntokens++;
@@ -76,7 +76,7 @@ static int set_input_boost_freq(const char *buf, const struct kernel_param *kp)
 			return -EINVAL;
 		for_each_possible_cpu(i)
 			per_cpu(sync_info, i).input_boost_freq = val;
-		goto check_enable;
+		goto out;
 	}
 
 	/* CPU:value pair */
@@ -95,15 +95,7 @@ static int set_input_boost_freq(const char *buf, const struct kernel_param *kp)
 		cp++;
 	}
 
-check_enable:
-	for_each_possible_cpu(i) {
-		if (per_cpu(sync_info, i).input_boost_freq) {
-			enabled = true;
-			break;
-		}
-	}
-	input_boost_enabled = enabled;
-
+out:
 	return 0;
 }
 
@@ -227,7 +219,7 @@ static int boost_mig_sync_thread(void *data)
 	unsigned long flags;
 
 	while(1) {
-		wait_eventwait_event_interruptible(s->sync_wq,
+		wait_event_interruptible(s->sync_wq,
  					s->pending || kthread_should_stop());
 
 		if (kthread_should_stop())
@@ -248,6 +240,7 @@ static int boost_mig_sync_thread(void *data)
 
 		if (src_policy.cur == src_policy.cpuinfo.min_freq) {
  			pr_debug("No sync. Source CPU%d@%dKHz at min freq\n",
+ 					src_cpu, src_policy.cur);
 			continue;
 		}
 
