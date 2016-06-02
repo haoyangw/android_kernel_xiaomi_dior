54f14d4cb7ccc487f1664f8564be4efc330b7999
Ashish Kotnala <ashishkotnala1.ak@gmail.com>
2016-06-02 23:17:53 +0530

    Revert "msm: thermal: add Thermal solution"
    This reverts commit c8638a82e2f1c761b1efc0021b698ca36cea8543.

--- arch/arm/mach-msm/board-8226.c
+++ arch/arm/mach-msm/board-8226.c
@@ -206,6 +206,7 @@ void __init msm8226_add_drivers(void)
 	fan53555_regulator_init();
 	cpr_regulator_init();
 	tsens_tm_init_driver();
+	msm_thermal_device_init();
 #ifdef CONFIG_ANDROID_RAM_CONSOLE
 	ram_console_debug_init();
 #endif
